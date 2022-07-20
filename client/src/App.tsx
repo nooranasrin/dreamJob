8794f68452cbfa530ce0c6764adab0cbf7053794
nooranasrinvc <73095150+nooranasrinvc@users.noreply.github.com>
2022-07-20 16:43:19 +0530

    moved header code into the Header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,16 +6,7 @@ import Jobs from './features/jobs/jobs';
 const App: React.FC = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <div className='bg-white'>
-        <div className='flex flex-row h-14 items-center justify-between m-auto w-8/12'>
-          <div></div>
-          <div className='flex font-bold text-base text-slate-600 justify-around w-1/6 '>
-            <Link to='/'>HOME</Link>
-            <Link to='jobs'>JOBS</Link>
-          </div>
-        </div>
-        <Header />
-      </div>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/jobs' element={<Jobs />} />
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Header: React.FC = (): JSX.Element => {
-  return <div className='bg-blue-600 h-56 rounded-bl-normal z-0'></div>;
+  return (
+    <div className='bg-white'>
+      <div className='flex flex-row h-14 items-center justify-between m-auto w-8/12'>
+        <div></div>
+        <div className='flex font-bold text-base text-slate-600 justify-around w-1/6 '>
+          <Link to='/'>HOME</Link>
+          <Link to='jobs'>JOBS</Link>
+        </div>
+      </div>
+      <div className='bg-blue-600 h-56 rounded-bl-normal z-0'></div>
+    </div>
+  );
 };
 
 export default Header;
